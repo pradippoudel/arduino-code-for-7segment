109ec23a70a4bf0457fb1bc11099248c0d1ce396
Pradip Poudel <50986372+pradippoudel@users.noreply.github.com>
2020-04-24 09:28:37 +0545

    Add files via upload

---
+++ simple_7_segment.ino
@@ -0,0 +1,22 @@
+void setup()
+{// put your setup code here, to run once:
+pinMode(2, OUTPUT); //a
+pinMode(3, OUTPUT); //b
+pinMode(4, OUTPUT); //c
+pinMode(5, OUTPUT); //d
+pinMode(6, OUTPUT); //e
+pinMode(7, OUTPUT); //f
+pinMode(8, OUTPUT); //g
+pinMode(9, OUTPUT); //dot
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, 1);
+   digitalWrite(3, 1);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 1);
+  digitalWrite(9, 1);
+}
